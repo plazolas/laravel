fb0939d5509a57db4bdcb59b41e94a6bbff19684
Oswald Plazola <plazolas@yahoo.com>
2018-07-25 22:52:31 +0100

    added phone to user registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -23,6 +23,20 @@
                                 @endif
                             </div>
                         </div>
+                        
+                        <div class="form-group{{ $errors->has('phone') ? ' has-error' : '' }}">
+                            <label for="phone" class="col-md-4 control-label">Phone</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="text" class="form-control" name="phone" value="{{ old('phone') }}">
+
+                                @if ($errors->has('phone'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('phone') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
@@ -37,7 +51,7 @@
                                 @endif
                             </div>
                         </div>
-
+                        
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                             <label for="password" class="col-md-4 control-label">Password</label>
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,10 +38,11 @@
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
+                                {{ Auth::user()->name }} <!-- oz quick fix span class="caret"></span -->
                             </a>
 
-                            <ul class="dropdown-menu" role="menu">
+                            <!-- ul class="dropdown-menu" role="menu"-->
+                            <ul>
                                 <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
                             </ul>
                         </li>
